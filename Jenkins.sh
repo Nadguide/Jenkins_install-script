a5af97e014661f361dbb14c9a2fe7e8408c60679
Nadguide <ntouayem@yahoo.com>
2022-10-26 19:04:55 -0500

    jenkins_install

---
+++ Jenkins.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Author: Nadine
+# Date: 10/26/2022
+
+# Jenkins installtion script.
+
+echo "begening jenkins installation"
+
+sudo yum install java-11-openjdk-devel -y
+
+curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
+
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+
+sudo yum install jenkins -y
+
+sudo systemctl start jenkins
+
+sudo systemctl status jenkins
+sudo systemctl enable jenkins
+sudo firewall-cmd --permanent --zone=public --add-port=8080/tcp
+sudo firewall-cmd --reload
+
+echo "end of jenkins installation" 
